787b3d73a2784273c61f7521fcfd07d39b8591a3
dira sanjaya wardana <dirawardana.dw@gmail.com>
2024-04-24 13:19:45 +0700

    Initial Commit

---
+++ VoiceAI/VoiceAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceAIApp.swift
+//  VoiceAI
+//
+//  Created by MACBOOK PRO on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
